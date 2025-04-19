59418ba665a0dd4e25e4f0b6ed0bb2b79fc74950
vgdhieu0602 <vg.dhieu0602@gmail.com>
2025-04-19 14:37:09 +0700

    change style emlement

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,19 @@
 import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Input } from "@/components/ui/input";
+import { Progress } from "@/components/ui/progress";
+import { Textarea } from "@/components/ui/textarea";
 
 export default function Home() {
   return (
-    <div>
-      <h1>Home</h1>
-      <Button variant="destructive">Hello</Button>
+    <div className="p-4">
+      <div className="flex flex-col gap-y-4">
+        <Button variant="elevated">This is button</Button>
+        <Input placeholder="This is input" />
+        <Progress value={50} />
+        <Textarea placeholder="Textarea" />
+        <Checkbox />
+      </div>
     </div>
   );
 }
